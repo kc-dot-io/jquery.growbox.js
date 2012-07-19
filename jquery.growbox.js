f4f958345735265d744c3d7c36daee45da2fffdb
Kyle Campbell <mail@slajax.com>
2012-07-19 14:40:11 -0700

    added a param for filter to allow for event delgation

--- jquery.growbox.js
+++ jquery.growbox.js
@@ -1,38 +1,42 @@
 
 if(typeof jQuery != "undefined" ) {
 
-    $.fn.growBox = function() {
+    (function($){
 
+        $.fn.growBox = function(options) {
 
-        var grow = function() {
+            options = $.extend({ filter: 'textarea'}, options);
 
-            var lh = Math.ceil( parseInt( $(this).css('line-height') ) )
-              , nl = $(this).val().split('\n').length
-              , cl = $(this).val().length
-              , cw = 8
-              , tw = $(this).width() / cw
-              , lines = Math.ceil( cl / tw + nl )
-              , nh = lines * lh + lh;
+            var grow = function() {
 
-            if( nh > lh * 12 ) {
-                $(this).css({ height: lh * 12 });
-                return $(this).css({'overflow-y':'auto'});
-            } else {
-                $(this).css({'overflow-y': 'hidden'});
-                return $(this).css({ height: nh });
+                var lh = Math.ceil( parseInt( $(this).css('line-height') ) )
+                  , nl = $(this).val().split('\n').length
+                  , cl = $(this).val().length
+                  , cw = 8
+                  , tw = $(this).width() / cw
+                  , lines = Math.ceil( cl / tw + nl )
+                  , nh = lines * lh + lh;
+
+                if( nh > lh * 12 ) {
+                    $(this).css({ height: lh * 12 });
+                    return $(this).css({'overflow-y':'auto'});
+                } else {
+                    $(this).css({'overflow-y': 'hidden'});
+                    return $(this).css({ height: nh });
+                }
             }
-        }
 
-        if ( typeof $(this).data('original-height') == "undefined"  ) {
-            $(this).css('resize','none')
-            $(this).data('original-height', $(this).outerHeight());
-            $(this).bind('keyup keypress focus',grow);
-        }
+            if ( typeof $(this).data('original-height') == "undefined"  ) {
+                $(this).css('resize','none')
+                $(this).data('original-height', $(this).outerHeight());
+                $(document).on('keyup keypress focus', options.filter, grow);
+            }
 
-        return grow.apply(this);
+            return grow.apply(this);
 
-    };
+        };
 
+    })(jQuery);
 
 }
 
