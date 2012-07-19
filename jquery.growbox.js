207bcc20ed88013bd576d73b6cc6ec32229fe1d4
Kyle Campbell <mail@slajax.com>
2012-07-19 12:22:56 -0700

    updated readme

--- jquery.growbox.js
+++ jquery.growbox.js
@@ -3,27 +3,36 @@ if(typeof jQuery != "undefined" ) {
 
     $.fn.growBox = function() {
 
-        var el = $(this).css('resize','none')
-          , oh = $(el).data('original-height');
 
-        if ( typeof oh == "undefined" )
-            $(el).data('original-height', $(el).outerHeight() );
+        var grow = function() {
 
-        var lh = Math.ceil( parseInt( $(el).css('line-height') ) )
-          , nl = $(el).val().split('\n').length
-          , cl = $(el).val().length
-          , cw = 8
-          , tw = $(el).width() / cw
-          , lines = Math.ceil( cl / tw + nl )
-          , nh = lines * lh + lh;
+            var lh = Math.ceil( parseInt( $(this).css('line-height') ) )
+              , nl = $(this).val().split('\n').length
+              , cl = $(this).val().length
+              , cw = 8
+              , tw = $(this).width() / cw
+              , lines = Math.ceil( cl / tw + nl )
+              , nh = lines * lh + lh;
 
-        if( nh > lh * 12 ) {
-            $(el).css({ height: lh * 12 });
-            return $(el).css({'overflow-y':'auto'});
-        } else {
-            $(el).css({'overflow-y': 'hidden'})
-            return el.css({ height: nh });
+            if( nh > lh * 12 ) {
+                $(this).css({ height: lh * 12 });
+                return $(this).css({'overflow-y':'auto'});
+            } else {
+                $(this).css({'overflow-y': 'hidden'});
+                return $(this).css({ height: nh });
+            }
         }
-    }
+
+        if ( typeof $(this).data('original-height') == "undefined"  ) {
+            $(this).css('resize','none')
+            $(this).data('original-height', $(this).outerHeight());
+            $(this).bind('keyup keypress focus',grow);
+        }
+
+        return grow.apply(this);
+
+    };
+
+
 }
 
